b6a97844edc9d319c04ea2faf4403d6bd446577e
Gangineni Ravi Teja <rteja7228@gmail.com>
2014-08-03 17:34:17 +0530

    Create supertables.php

---
+++ supertables.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Read input from stdin and provide input before running code
+
+fscanf(STDIN, "%s\n", $name);
+echo "Hi, ".$name;
+*/
+function table($num1, $num2)
+    {
+    	$tarray = array(); 
+    	if($num1==1)
+            return 1;
+     for  ($k=1; $k<=$num2; $k++){
+     	 
+               	 
+     	  $tarray[$k-1]= $num1*$k;
+     	
+           }
+     return $tarray; 
+     
+    }
+
+ $t=trim(fgets(STDIN));
+ 
+    for($k=1;$k<=$t;$k++)
+    {
+        $i=fgets(STDIN);
+        
+        global $n ;
+        $r = $i.explode(" ", $i);
+        $a = $r[0];
+        $b = $r[2];
+        $n = $r[4];
+     $A = table($a, $n);
+     $B = table($b, $n);
+     $unique = array_unique(array_merge($A,$B)); 
+      sort($unique);
+     echo  $unique[$n-1]."\n";
+    }
+
+
+?>
